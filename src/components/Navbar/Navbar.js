8395ffb43016bd164e7f07b46744f36e188e9180
sansar90 <sansargarg42@gmail.com>
2021-04-06 12:29:19 +0530

    1 file modified

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
 
-//import memories from '../../images/memories.png';
+
 import * as actionType from '../../constants/actionTypes';
 import useStyles from './styles';
 
@@ -39,7 +39,7 @@ const Navbar = () => {
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
         <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">MemoriesBook</Typography>
-        <img className={classes.image} href="memories.png" alt="icon" height="60" />
+        <img className={classes.image} src="https://stackblitz.com/edit/react-adccjp?file=public%2Fmemories.png" alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>
         {user?.result ? (
