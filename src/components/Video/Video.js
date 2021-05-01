69befc70ca84c043a16a3da3ec174e1d4aab568d
sansar90 <sansargarg42@gmail.com>
2021-05-01 15:55:07 +0530

    2 files modified, 1 added

---
+++ src/components/Video/Video.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
+
+import "./Video.css";
+
+
+
+const dashboard = React.memo(() => {
+    
+    const [videoList, setVideoList] = useState([]);
+    useEffect(() => {
+       
+            axios
+                .get("http://localhost:3000/videoList", {
+                    headers: {
+                        "Content-Type": "application/json"
+                        
+                    },
+                })
+                .then((res) => {
+                    setVideoList(res.data);
+                });
+        
+    }, []);
+    
+    const videos = videoList.map((video) => {
+
+        return (
+            <div
+                className="video col-xs-12 col-sm-12 col-md-3 col-lg-4"
+                key={video._id}
+            >
+                <Link to={"/video/" + video.uploadTitle}>
+                    <div className="video-thumbnail">
+                        <img src={video.thumbnailPath} alt="video thubmnail" />
+                    </div>
+                </Link>
+                <span className="username">
+                    <Link to={"/videos/" + video.uploadTitle}>
+                        {video.uploaderName}
+                    </Link>
+                </span>
+                <span className="video-title">
+                    {video.uploadTitle}
+                </span>
+            </div>
+        );
+    });
+
+    return (
+        <React.Fragment>
+            
+            <div className="container mt-5">
+                <h4>Videos</h4>
+                <hr className="my-4" />
+
+                <div className="streams row">{videos}</div>
+            </div>
+        </React.Fragment>
+    );
+});
+
+export default dashboard;
